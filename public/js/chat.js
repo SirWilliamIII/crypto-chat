fd818b87da9016a660138e2e013c565a413d4536
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-06-22 15:02:54 -0500

    fixed socket script

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,39 +1,50 @@
-const socket = io()
+const socket = io();
 
-socket.on('connect', function() {
-	const params = jQuery.deparam(window.location.search)
+socket.on("connect", function () {
+  const params = deparam(window.location.search);
 
-	socket.emit('join', params, (err, data) => {
-		if (err) {
-			alert(err)
-			window.location.href = '/'
-		} else {
-			console.log('No error')
-		}
-	})
-})
+  function deparam(queryString) {
+    const params = {};
+    const pairs = queryString.slice(1).split("&");
+    for (let i = 0; i < pairs.length; i++) {
+      const pair = pairs[i].split("=");
+      const key = decodeURIComponent(pair[0]);
+      const value = decodeURIComponent(pair[1] || "");
+      params[key] = value;
+    }
+    return params;
+  }
 
-socket.on('disconnect', function() {
-	console.log('Disconnected from server')
-})
+  socket.emit("join", params, (err, data) => {
+    if (err) {
+      alert(err);
+      window.location.href = "/";
+    } else {
+      console.log("No error");
+    }
+  });
+});
 
-socket.on('newMessage', function(message) {
-	console.log('New message', message)
-	const li = jQuery('<li></li>')
-		li.text(`${message.from}: ${message.text}`)
-		jQuery('#messages').append(li)
-})
+socket.on("disconnect", function () {
+  console.log("Disconnected from server");
+});
 
-jQuery('#message-form').on('submit', evt => {
-	evt.preventDefault()
-
-	socket.emit('createMessage', {
-		from: 'User',
-		text: jQuery('[name=message]').val()
-	}, () => {
-
-	})
-
-})
-
-
+socket.on("newMessage", function (message) {
+  console.log("New message", message);
+  const li = document.createElement("li");
+  li.textContent = `${message.from}: ${message.text}`;
+  document.getElementById("messages").appendChild(li);
+});
+document
+  .getElementById("message-form")
+  .addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    socket.emit(
+      "createMessage",
+      {
+        from: "User",
+        text: document.querySelector("[name=message]").value,
+      },
+      function () {}
+    );
+  });
