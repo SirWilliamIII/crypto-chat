5841f0b7b2d81c385db383b6a2ab21164d57eecc
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-06-25 18:34:38 -0500

    refactored

--- public/js/libs/deparams.js
+++ public/js/libs/deparams.js
@@ -1,17 +1,19 @@
-(function($) {
-    $.deparam = $.deparam || function(uri) {
-        const salt = "will"
-        if (uri === undefined) {
-            uri = window.location.search
+(function () {
+  window.deparam =
+    window.deparam ||
+    function (uri) {
+      const salt = "will";
+      if (uri === undefined) {
+        uri = window.location.search;
+      }
+      const queryString = {};
+      uri.replace(
+        new RegExp("([^?=&]+)(=([^&#]*))?", "g"),
+        function ($0, $1, $2, $3) {
+          queryString[$1] = decodeURIComponent($3.replace(/\+/g, "%20"));
         }
-        const queryString = {}
-        uri.replace(
-            new RegExp('([^?=&]+)(=([^&#]*))?', 'g'),
-            ($0, $1, $2, $3) => {
-                queryString[$1] = decodeURIComponent($3.replace(/\+/g, '%20'))
-            }
-        );
-        queryString.room += salt
-        return queryString
+      );
+      queryString.room += salt;
+      return queryString;
     };
-}(jQuery))
+})();
